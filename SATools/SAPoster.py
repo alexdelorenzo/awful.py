f4d60e40428054396ec160fe6eb96d45f2a37974
alex <alex.delorenzo@gmail.com>
2013-12-23 01:53:08 -0500

    add reply to thread action

--- SATools/SAPoster.py
+++ SATools/SAPoster.py
@@ -3,6 +3,7 @@ __author__ = 'alex'
 class SAPoster(object):
 	def __init__(self, id, name, session):
 		self.id = id
+		self.url = "http://forums.somethingawful.com/member.php?action=getinfo&userid=" + self.id
 		self.name = name
 		self.session = session
 
--- SATools/SASession.py
+++ SATools/SASession.py
@@ -1,7 +1,7 @@
 __author__ = 'alex'
-
+from SATools.SAPoster import SAPoster
 import requests
-
+from bs4 import BeautifulSoup
 
 class SASession(requests.Session):
 	def __init__(self, username, passwd):
@@ -9,12 +9,13 @@ class SASession(requests.Session):
 		self.username = username
 		self.base_url = 'https://forums.somethingawful.com/'
 		self.login(username, passwd)
+		self.id = self.cookies.get('bbuserid')
+		self.profile = SAPoster(self.id, username, self)
 
 
 	def login(self, username, passwd):
 		login_url = self.base_url + 'account.php'
 		post_data = {'username': username, 'password': passwd, 'action': 'login'}
-
 
 		response = self.post(login_url, post_data)
 
@@ -22,6 +23,22 @@ class SASession(requests.Session):
 			raise Exception((response.status_code, response.reason))
 
 
+	def post_thread(self, title, body, tag=None, poll=None):
+		pass
+
+	def reply(self, id, body):
+		url = "http://forums.somethingawful.com/newreply.php?action=newreply&threadid=" + str(id)
+
+		response = self.get(url)
+		bs = BeautifulSoup(response.content)
+
+		want = 'action', 'formkey', 'form_cookie'
+		inputs = {i['name']: i['value'] for i in bs.find_all('input') if i.has_key('value') and i['name'] in want}
+		inputs['parseurl'] = 'yes'
+		inputs['message'] = body
+
+		response = self.post(url, inputs)
+		return response
 
 
 

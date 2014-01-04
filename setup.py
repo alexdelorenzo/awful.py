898a565556dd7b74dc391e42fa12fd394e7608a2
alex <alex.delorenzo@gmail.com>
2014-01-03 19:51:11 -0500

    init commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name='AwfulPy',
+	version='0.0.0.0.0.1',
+	packages=['SATools', 'AwfulGUI'],
+	url='https://github.com/thismachinechills/AwfulPy',
+	license='Modified Gippel; see readme',
+	author='alex',
+	author_email='',
+	description='AwfulPy - forums dot somethingawful dot com api',
+  long_description='use pythong to read and make bad posts'
+)

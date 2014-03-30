2d51d273d0ecf41cfcf9cca9531b9962bd2228fa
alex <alex.delorenzo@gmail.com>
2014-03-29 17:06:03 -0700

    updated semantics

--- SATools/SASession.py
+++ SATools/SASession.py
@@ -38,8 +38,8 @@ class SASession(object):
         if not response.ok:
             raise Exception(("Unable to login", response.status_code, response.reason))
 
-    def reply(self, id, body):
-        url = "http://forums.somethingawful.com/newreply.php?action=newreply&threadid=" + str(id)
+    def reply(self, _id, body):
+        url = "http://forums.somethingawful.com/newreply.php?action=newreply&threadid=" + str(_id)
 
         response = self.session.get(url)
         bs = BeautifulSoup(response.content)
@@ -60,9 +60,6 @@ class SASession(object):
 
     def find_user_posts(self, user_id):
         raise NotImplementedError()
-        search = SASearch(query=user_id, session=self.session)
-        search.search_userid(user_id)
-        return search
 
     def search(self):
         raise NotImplementedError()

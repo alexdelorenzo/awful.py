9068c20331d6fc6962c70f200dfdfae664d6c88d
alex <alex.delorenzo@gmail.com>
2014-05-03 22:23:32 +0000

    hello, my name is alex and i have faulty assumptions about python's super() and MI.

---
+++ SATools/SAObjs/SABase.py
@@ -0,0 +1,28 @@
+class SABase(object):
+    def __init__(self, parent, *args, **kwargs):
+        self.parent = parent
+
+    @staticmethod
+    def _is_protected(string):
+        under = '_'
+        first_under = string[0] == under
+
+        return first_under
+
+    @staticmethod
+    def _is_magic(string, dunder='__'):
+        first_two_dunder = SABase._is_protected(string)
+
+        if not first_two_dunder:
+            return first_two_dunder
+
+        last_two_dunder = string[-2:] == dunder
+        is_magic = first_two_dunder and last_two_dunder
+
+        return is_magic
+
+    @staticmethod
+    def _is_private(string, dunder='__'):
+        first_two_dunder = string[:2] == dunder
+
+        return first_two_dunder

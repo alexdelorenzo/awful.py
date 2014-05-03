ab83a68bf64a6b53ffa8039d1cbc957de572b673
alex <alex.delorenzo@gmail.com>
2014-05-03 22:36:21 +0000

    classin' up the loose methods sprawled about w/o a concern in the world for how convoluted it might get.

--- SATools/SAParsers/ParserMap.py
+++ SATools/SAParsers/ParserMap.py
@@ -1,7 +1,11 @@
 from os import listdir
 
+sa_prefix = 'SA'
+prefix_index = len(sa_prefix)
 
-prefix_index = len('SA')
+sa_tools = listdir('..')
+sa_parsers = listdir('../SAParsers')
+
 
 
 def index_of(file_name, find_str='.'):
@@ -16,6 +20,9 @@ def prefix(file_name, prefix_str='SA'):
     if prefix_str is not None:
         prefix_index = len(prefix_str)
 
+    else:
+        return 0
+
     return file_name[:prefix_index]
 
 
@@ -26,20 +33,32 @@ def suffix(file_name, suffix_str=None):
     else:
         index = index_of(file_name)
 
-    return file_name[index:]
+    if index is None:
+        return len(file_name)
+
+    else:
+        return file_name[index:]
 
 
-def noun(file_name):
-    if suffix(file_name):
-        suffix_index = index_of(file_name)
-        file_name = file_name[prefix_index:suffix_index]
+def noun(file_name, prefix_str='SA', suffix_str='Parser'):
+    prefix_index = index_of(file_name, prefix_str)
+    suffix_index = index_of(file_name, suffix_str)
 
-    parser_str = 'Parser'
-    if parser_str in file_name:
-        parser_index = index_of(file_name, parser_str)
-        file_name = file_name[:parser_index]
+    return file_name[prefix_index:suffix_index]
 
-    return file_name
+
+#
+# def noun(file_name):
+#     if suffix(file_name):
+#         suffix_index = index_of(file_name)
+#         file_name = file_name[prefix_index:suffix_index]
+#
+#     parser_str = 'Parser'
+#     if parser_str in file_name:
+#         parser_index = index_of(file_name, parser_str)
+#         file_name = file_name[:parser_index]
+#
+#     return file_name
 
 
 def is_py(file_name):
@@ -81,16 +100,54 @@ def first_occurrence(item, sequence):
     return False
 
 
-sa_tools = listdir('..')
-sa_parsers = listdir('../SAParsers')
+def get_names(file_list):
+    names = sorted(gen_sa_modules(file_list))
+    return names
 
-module_names = sorted(gen_sa_modules(sa_tools))
-parser_names = sorted(gen_sa_modules(sa_parsers))
 
-sa_module_nouns = module_nouns(module_names)
-sa_parser_nouns = module_nouns(parser_names)
+def get_name_map(names):
+    nouns = module_nouns(names)
+    name_map = dict(zip(nouns, names))
+    return name_map
 
-sa_modules_map = dict(zip(sa_module_nouns, module_names))
-sa_parser_map = dict(zip(sa_parser_nouns, parser_names))
 
-#
+def get_parser_map():
+    module_names = get_names(sa_tools)
+    parser_names = get_names(sa_parsers)
+
+    sa_modules_map = get_name_map(module_names)
+    sa_parser_map = get_name_map(parser_names)
+
+    parser_map = dict()
+
+    for module_noun in sa_modules_map:
+        if module_noun in sa_parser_map:
+            parser_noun = sa_modules_map[module_noun]
+            module_noun = 'SA' + module_noun
+
+        else:
+            continue
+
+        parser_map[module_noun] = parser_noun
+
+    return parser_map
+
+
+def import_statement(parser_name):
+    package = "from SATools.SAParsers." + parser_name
+    import_cmd = "import " + parser_name
+
+    statement = package + ' ' + import_cmd
+
+    return statement
+
+
+def get_import_statement_map(parser_map=None):
+    if not parser_map:
+        parser_map = get_parser_map()
+
+    import_map = {module_noun: import_statement(parser_name)
+                  for module_noun, parser_name in parser_map.items()}
+
+    return import_map
+

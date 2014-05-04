e7f5c2c63a8884cbee7c4988b3cf525531f30524
alex <alex.delorenzo@gmail.com>
2014-05-04 21:01:39 +0000

    pull the parser dispatcher into its own module

--- SATools/SAParsers/SAParser.py
+++ SATools/SAParsers/SAParser.py
@@ -1,29 +1,11 @@
-from SATools.SAObjs.SAObj import SAObj, SAMagic
+from SATools.SAObjs.SAObj import SAObj
 from SATools.SAParsers.BSWrapper import BSWrapper
+from SATools.SAParsers.ParserDispatch import ParserDispatch
 
 
-class Dispatch(SAMagic):
-    def __init__(self, parent, parser_map=None, **kwargs):
-        super(Dispatch, self).__init__(parent, **kwargs)
-        self._parser_map = None
-        self.set_parser_map(parser_map)
-
-    def set_parser_map(self, parser_map=None):
-        if parser_map is None:
-            parser_map = dict()
-
-        self._parser_map = parser_map
-
-    def dispatch(self, key, val=None, content=None):
-        if key not in self._parser_map:
-            return
-
-        self._parser_map[key](key, val, content)
-
-
-class SAParser(SAObj, Dispatch):
+class SAParser(SAObj, ParserDispatch):
     def __init__(self, parent, wrapper=BSWrapper, parser_map=None, *args, **kwargs):
-        super(SAParser, self).__init__(parent, *args, **kwargs)
+        super(SAParser, self).__init__(parent, *args, parser_map=parser_map, **kwargs)
         self.id = self.parent.id
         self.wrapper = None
         self.set_wrapper(wrapper)

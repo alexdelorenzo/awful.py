14d48793c27fc107991f672d4731a907cf2dd2b9
alex <alex.delorenzo@gmail.com>
2014-05-04 21:02:47 +0000

    formatting and names

--- SATools/SAParsers/SAThreadParser.py
+++ SATools/SAParsers/SAThreadParser.py
@@ -14,6 +14,7 @@ class SAThreadParser(SAParser, RegexManager):
         super(SAThreadParser, self).parse()
         self.parse_info()
         self.parse_posts()
+
         self._delete_extra()
 
     def parse_info(self):
@@ -30,29 +31,11 @@ class SAThreadParser(SAParser, RegexManager):
             post_id = post['id'][4:]
             self.parent._add_post(post_id, post)
 
-    def _parse_from_url(self):
-        self.parent.read()
-        self._parse_first_post()
-        title = self.content.find('a', 'bclast').text.strip()
-        self.parent.title = title
-
-    def _parse_first_post(self, post_content=None):
-        if not post_content:
-            post_content = self.content.find('table', 'post')
-
-        post_id = post_content['id'][4:]
-
-        #TODO: pull this out into SAThread._add_first_post()
-        self.parent._add_post(post_id, post_content)
-        sa_post = self.parent.posts.popitem(0)[-1]
-        self.parent.poster = sa_post.poster
-
-
     def set_parser_map(self, parser_map=None):
         if not parser_map:
             parser_map = \
                 {'icon': self._parse_icon,
-                 'lastpost': self._parse_lastpost,
+                 'lastpost': self._parse_last_post,
                  'replies': self._parse_replies,
                  'author': self._parse_author,
                  'title': self._parse_title,
@@ -83,6 +66,23 @@ class SAThreadParser(SAParser, RegexManager):
 
         super(SAThreadParser, self).set_regex_strs(regex_strs)
 
+    def _parse_from_url(self):
+        self.parent.read()
+        self._parse_first_post()
+        title = self.content.find('a', 'bclast').text.strip()
+        self.parent.title = title
+
+    def _parse_first_post(self, post_content=None):
+        if not post_content:
+            post_content = self.content.find('table', 'post')
+
+        post_id = post_content['id'][4:]
+
+        #TODO: pull this out into SAThread._add_first_post()
+        self.parent._add_post(post_id, post_content)
+        sa_post = self.parent.posts.popitem(0)[-1]
+        self.parent.poster = sa_post.poster
+
     def _parse_tr_thread(self):
         if not self.content:
             return
@@ -95,22 +95,25 @@ class SAThreadParser(SAParser, RegexManager):
 
     def _parse_icon(self, key, val, content):
         text = content.a['href'].split('posticon=').pop(-1)
+
         setattr(self.parent, key, text)
 
-    def _parse_lastpost(self, key, val, content):
+    def _parse_last_post(self, key, val, content):
         groups = 'time', 'date', 'user'
         matches = self.regex_matches(key, val)
         matches = dict(zip(groups, matches))
+
         setattr(self.parent, key, matches)
 
     def _parse_author(self, key, val, content):
         link = content.a
         author = link.text.strip()
         user_id = link['href'].split('id=')[-1]
+
         self.parent._add_author(user_id, author)
 
     def _parse_replies(self, key, val, content):
-        self._parse_pagecount(val)
+        self._parse_page_count(val)
         link = content.a
 
         if link:
@@ -122,6 +125,7 @@ class SAThreadParser(SAParser, RegexManager):
 
     def _parse_views(self, key, val, content):
         views = int(content.text.strip())
+
         setattr(self.parent, key, views)
 
     def _parse_rating(self, key, val, content):
@@ -138,20 +142,22 @@ class SAThreadParser(SAParser, RegexManager):
             rating = {'votes': votes,
                       'avg': avg,
                       'stars': stars}
+
             setattr(self.parent, key, rating)
 
     def _parse_title(self, key, val, content):
         text = content.find('a', 'thread_title').text
         key = 'title'
 
-        self._parse_lastseen(content)
+        self._parse_last_seen(content)
         setattr(self.parent, key, text)
 
-    def _parse_lastseen(self, content):
+    def _parse_last_seen(self, content):
         last_read = content.find('div', 'lastseen')
         self.parent._add_last_read(last_read)
 
-    def _parse_pagecount(self, val):
+    def _parse_page_count(self, val):
         pages = ceil(int(val) / 40.0)
         key = 'pages'
+
         setattr(self.parent, key, pages)

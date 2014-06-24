3268dc00401352a1bce3bcc81eae7c408b12be69
alex <alex.delorenzo@gmail.com>
2014-06-23 21:22:54 -0400

    implement methods needed to parse new parser.results dict()

--- sa_tools/thread.py
+++ sa_tools/thread.py
@@ -8,6 +8,8 @@ from sa_tools.last_read import SALastRead
 
 from collections import OrderedDict as ordered
 
+from fn import _
+
 
 class SAThread(SAListObj):
     posts = TriggerProperty('read', 'posts')
@@ -33,16 +35,6 @@ class SAThread(SAListObj):
         self._set_results()
         self._delete_extra()
 
-    def _set_results(self):
-        post_gen = self.parser.post_gen
-
-        for post_info in post_gen:
-            self._add_post(*post_info)
-
-        for name in self.parser.parser_map.keys():
-            attr = getattr(self.parser, name)
-            setattr(self, name, attr)
-
     def _add_post(self, post_id, post_content, is_op=False):
         sa_post = SAPost(self, post_id, post_content)
         post_id = sa_post.id
@@ -54,11 +46,33 @@ class SAThread(SAListObj):
             self.author = poster
 
     def _add_last_read(self, lr_content, lr=None):
+        print('lr1')
         if lr_content:
             lr = SALastRead(self, self.id, lr_content)
+            print('lr')
 
         self.last_read = lr
 
     def _add_author(self, user_id, name=None):
         sa_poster = SAPoster(self, user_id, name=name)
         self.author = sa_poster
+
+    def _apply_parsed_results(self):
+        condition_map = {'author': expand(self._add_author),
+                         'last_read': self._add_last_read}
+
+        super(SAThread, self)._apply_parsed_results(condition_map=condition_map)
+
+    def _set_results(self):
+        post_gen = self.parser.post_gen
+
+        for post_info in post_gen:
+            self._add_post(*post_info)
+
+        self._apply_parsed_results()
+
+
+def expand(func):
+    def new(arg):
+        func(*arg)
+    return new
